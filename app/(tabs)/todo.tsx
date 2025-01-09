c0ea25e291f97ebcbe05c46f39a623bc622a56ab
Raxuis <raclot.raphael@gmail.com>
2025-01-09 14:43:25 +0100

    feat(tasks): add toggle to mark a task completed

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -2,8 +2,7 @@ import {ThemedText} from "@/components/ThemedText";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {ScrollView, View} from "react-native";
-import {Checkbox, CheckboxGroup, CheckboxIcon, CheckboxIndicator} from "@/components/ui/checkbox";
-import {IconSymbol} from "@/components/ui/IconSymbol";
+import {Checkbox, CheckboxIcon, CheckboxIndicator} from "@/components/ui/checkbox";
 
 type Task = {
     userId: string;
@@ -25,6 +24,13 @@ export default function Todo() {
             .finally(() => setIsLoading(false));
         return (): void => setTasks([]);
     }, []);
+
+    const onToggle = (taskId: string) => {
+        setTasks((prevTasks: Task[]) => prevTasks.map((task) => task.id === taskId
+            ? {...task, completed: !task.completed} : task
+        ));
+    }
+
     return (
         <View className="flex flex-col min-h-screen min-w-screen py-20 px-8 text-black overflow-hidden">
             <ThemedText type="title" className="font-bold">Todo App</ThemedText>
@@ -36,9 +42,12 @@ export default function Todo() {
                         tasks.map((task: Task) => (
                             <View key={task.id} className="flex flex-row items-center gap-2">
                                 <View>
-                                    <Checkbox value={task.title} isChecked={task.completed}>
-                                        <CheckboxIndicator size="md">
-                                            <CheckboxIcon size="md"/>
+                                    <Checkbox value={task.title}
+                                              isChecked={task.completed}
+                                              onChange={() => onToggle(task.id)}
+                                    >
+                                    <CheckboxIndicator size="md">
+                                            <CheckboxIcon/>
                                         </CheckboxIndicator>
                                     </Checkbox>
                                 </View>
