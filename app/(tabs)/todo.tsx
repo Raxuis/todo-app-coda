a7ac3227edaf4fbe230a37e2e0d05d9a800ef070
Raxuis <raclot.raphael@gmail.com>
2025-01-09 14:05:48 +0100

    feat(tasks): add fetch of tasks

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -1,13 +1,40 @@
 import {ThemedView} from "@/components/ThemedView";
 import {ThemedText} from "@/components/ThemedText";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Animated, ScrollView} from "react-native";
+
+type Task = {
+    userId: string;
+    id: string;
+    title: string;
+    completed: boolean;
+}
 
 export default function Todo() {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    useEffect(() => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/todos")
+            .then((response) => {
+                setTasks((prevTasks) => [...prevTasks, ...response.data]);
+            })
+            .catch((e) => console.error(e))
+            .finally(() => setIsLoading(false));
+    }, []);
     return (
-        <ThemedView className="flex flex-col min-h-screen min-w-screen pt-20 px-8 text-black overflow-hidden">
-            <ThemedText type="title" className="font-bold">
-                Todo App
-            </ThemedText>
-        </ThemedView>
+        <ScrollView className="flex flex-col min-h-screen min-w-screen pt-20 px-8 text-black overflow-hidden">
+            <ThemedText type="title" className="font-bold">Todo App</ThemedText>
+            {isLoading ? (
+                <ThemedText>Loading...</ThemedText>
+            ) : (
+                tasks.map((task:Task) => (
+                    <ThemedText key={task.id} className="mt-2">
+                        {task.title}
+                    </ThemedText>
+                ))
+            )}
+        </ScrollView>
     );
 };
