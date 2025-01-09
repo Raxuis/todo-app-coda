14f30fab1734310316fd82d803ef69f1f4dbd4cd
Raxuis <raclot.raphael@gmail.com>
2025-01-09 12:28:32 +0100

    chore(nativewind): add nativewind

---
+++ app/(tabs)/todo.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+export default function Todo() {
+    return (
+        <View>
+            <Text className="bg-red-500">
+                Test
+            </Text>
+        </View>
+    );
+};
